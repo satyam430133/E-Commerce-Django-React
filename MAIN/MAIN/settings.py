e813a041295b0100d23cf38ec1213318f48703e8
satyam430133 <satyambadal07@gmail.com>
2025-01-07 15:02:31 +0530

    pk

--- MAIN/MAIN/settings.py
+++ MAIN/MAIN/settings.py
@@ -15,12 +15,11 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-h(q4f5+k*t%$e^sz9d3^)^!ekmuh9zy8w-v3kd6et^oa^b3yu2'
+SECRET_KEY = 'django-insecure-m@mcy)%0&!gn#l&718+7!f*8ourxcdr#9)s+m$_rgljmatyl+*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +36,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'store',
 ]
 
 MIDDLEWARE = [
